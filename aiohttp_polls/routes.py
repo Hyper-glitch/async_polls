92a816207e0b5fffa4a8f302c9ae008061d900c5
hyper-glitch <graff049@gmail.com>
2022-08-19 17:08:34 +0300

    feature(static): set up static dir

--- aiohttp_polls/routes.py
+++ aiohttp_polls/routes.py
@@ -1,5 +1,7 @@
+from aiohttp_polls.setting import BASE_DIR
 from views import index
 
 
 def setup_routes(app):
     app.router.add_get('/', index)
+    app.router.add_static('/static/', path=BASE_DIR / 'static', name='static')
