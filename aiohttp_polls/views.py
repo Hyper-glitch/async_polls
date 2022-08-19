cae499b778c986ea45e584f8abd4de45bef93bbe
hyper-glitch <graff049@gmail.com>
2022-08-19 16:45:52 +0300

    feature(fetch): get questions from database

--- aiohttp_polls/views.py
+++ aiohttp_polls/views.py
@@ -1,5 +1,10 @@
 from aiohttp import web
+import db
 
 
 async def index(request):
-    return web.Response(text='Hello Aiohttp!')
+    async with request.app['db'].acquire() as connection:
+        cursor = await connection.execute(db.question.select())
+        records = await cursor.fetchall()
+        questions = [dict(question) for question in records]
+        return web.Response(text=str(questions))
