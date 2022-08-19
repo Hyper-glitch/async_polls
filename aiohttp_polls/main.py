aefc7bcd8a5e9e6fc1ee9a4059bc853d89bf02bb
hyper-glitch <graff049@gmail.com>
2022-08-19 18:00:00 +0300

    feature(middleware): add errors middleware

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -3,6 +3,7 @@ import jinja2
 from aiohttp import web
 
 from aiohttp_polls.db import add_db_engine
+from aiohttp_polls.middlewares import setup_middlewares
 from aiohttp_polls.setting import postgres_config, BASE_DIR
 from routes import setup_routes
 
@@ -12,5 +13,6 @@ app['config'] = postgres_config
 aiohttp_jinja2.setup(
     app=app, loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttp_polls' / 'templates'))
 )
+setup_middlewares(app)
 app.cleanup_ctx.append(add_db_engine)
 web.run_app(app)
