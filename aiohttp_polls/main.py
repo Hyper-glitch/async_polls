0b23a0165c27d7c3c1d7d53303a8ea4da048a251
hyper-glitch <graff049@gmail.com>
2022-08-19 16:08:21 +0300

    feature(engine): add db engine

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -1,9 +1,11 @@
 from aiohttp import web
 
+from aiohttp_polls.db import add_db_engine
 from aiohttp_polls.setting import postgres_config
 from routes import setup_routes
 
 app = web.Application()
 setup_routes(app)
 app['config'] = postgres_config
+app.cleanup_ctx.append(add_db_engine)
 web.run_app(app)
