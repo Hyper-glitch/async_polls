bed915a21b3cb0b4a7d766afafff27c45a86b624
hyper-glitch <graff049@gmail.com>
2022-08-19 16:56:15 +0300

    feature(jinja2): set up aiohttp jinja2

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -1,11 +1,16 @@
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
 
 from aiohttp_polls.db import add_db_engine
-from aiohttp_polls.setting import postgres_config
+from aiohttp_polls.setting import postgres_config, BASE_DIR
 from routes import setup_routes
 
 app = web.Application()
 setup_routes(app)
 app['config'] = postgres_config
+aiohttp_jinja2.setup(
+    app=app, loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttpdemo_polls' / 'templates'))
+)
 app.cleanup_ctx.append(add_db_engine)
 web.run_app(app)
--- aiohttp_polls/views.py
+++ aiohttp_polls/views.py
@@ -1,10 +1,12 @@
-from aiohttp import web
+import aiohttp_jinja2
+
 import db
 
 
+@aiohttp_jinja2.template('index.html')
 async def index(request):
     async with request.app['db'].acquire() as connection:
         cursor = await connection.execute(db.question.select())
         records = await cursor.fetchall()
         questions = [dict(question) for question in records]
-        return web.Response(text=str(questions))
+        return {'questions': questions}
