61fbd88a3921a2dc8a1a865721d8344c07d29e9e
hyper-glitch <graff049@gmail.com>
2022-08-19 14:54:45 +0300

    feature(config): set up config file

--- aiohttp_polls/main.py
+++ aiohttp_polls/main.py
@@ -1,7 +1,9 @@
 from aiohttp import web
 
+from aiohttp_polls.setting import postgres_config
 from routes import setup_routes
 
 app = web.Application()
 setup_routes(app)
+app['config'] = postgres_config
 web.run_app(app)
